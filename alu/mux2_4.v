f9ba1b56850dc35212162549d7526ddfbf91df1e
AlbertoLagoFdez <alu0101562247@ull.edu.es>
2023-10-19 16:51:36 +0100

    19/10

--- alu/mux2_4.v
+++ alu/mux2_4.v
@@ -1,9 +1,6 @@
 //error
 module mux2_4(output wire [3:0] Out, input wire [3:0] A, input wire [3:0] B, input wire s);
-  always @(A or B or s) begin
-    case (s)
-    1'b0 Out = A;
-    1'b1 Out = B;
-    endcase
-  end
+
+  assign Out = s ? B : A;
+
 endmodule
